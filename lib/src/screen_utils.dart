68491c7660f6a9452fef3ce27bac1b0f43d67b27
Tatenda <tathove@gmail.com>
2022-02-13 09:48:44 +0200

    add universal platform package

--- lib/src/screen_utils.dart
+++ lib/src/screen_utils.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:universal_platform/universal_platform.dart';
 
-abstract class ScreenUtils{
-  static bool isLargeScreen(BuildContext context){
+abstract class ScreenUtils {
+  static bool isLargeScreen(BuildContext context) {
     return MediaQuery.of(context).size.width > 758;
   }
+
 }
