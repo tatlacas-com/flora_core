e2dbac3e3d19bf03fdcc323b7f7ba6c4108368cc
Tatenda <tathove@gmail.com>
2022-02-23 09:41:58 +0200

    Update list_container.dart

--- lib/src/widgets/list_container.dart
+++ lib/src/widgets/list_container.dart
@@ -6,7 +6,6 @@ import 'package:tatlacas_flutter_core/src/widgets/zero_height_app_bar.dart';
 import 'items_list.dart';
 
 class ListContainer<TBloc extends ItemsManagerBloc> extends StatelessWidget {
-  final TBloc Function(BuildContext context) bloc;
   final PreferredSizeWidget? appBar;
   final ItemsListState<TBloc> Function() listStateBuilder;
   final Key? listBuilderKey;
@@ -14,20 +13,20 @@ class ListContainer<TBloc extends ItemsManagerBloc> extends StatelessWidget {
   const ListContainer({
     Key? key,
     this.listBuilderKey,
-    required this.bloc,
     required this.listStateBuilder,
     this.appBar,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final bloc = context.read<TBloc>();
+    if (bloc.state is ItemsLoadingState) {
+      bloc.add(LoadItemsEvent(context: context));
+    }
     return Scaffold(
       appBar: appBar ?? ZeroHeightAppBar(),
-      body: BlocProvider(
-        create: (context) =>
-            bloc.call(context)..add(LoadItemsEvent(context: context)),
-        child: ItemsList<TBloc>(key: listBuilderKey, stateBuilder: listStateBuilder),
-      ),
+      body:
+          ItemsList<TBloc>(key: listBuilderKey, stateBuilder: listStateBuilder),
     );
   }
 }
