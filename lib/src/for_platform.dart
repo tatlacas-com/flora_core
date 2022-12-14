317796d79dba8eac76014c5c03a1ff5c8b52b477
Tatenda Caston Hove <tathove@tatlacas.com>
2022-12-14 17:48:43 +0200

    Update for_platform.dart

--- lib/src/for_platform.dart
+++ lib/src/for_platform.dart
@@ -1,9 +1,3 @@
-enum ForPlatform{
-  None,
-  Android,
-  iOS,
-  Web,
-  Windows,
-  Linux,
-  MacOS
-}
+// ignore_for_file: constant_identifier_names
+
+enum ForPlatform { None, Android, iOS, Web, Windows, Linux, MacOS }
